cf8335509ff225d3c88e7c23d543597c3c21cc98
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-04-17 14:29:41 +0700

    add enum action type

---
+++ EpiserverCms.Web/Models/Constant/ActionTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverCms.Web.Models.Constant
+{
+    public enum ActionTypes
+    {
+        Deleted = 1,
+        Restore = 2
+    }
+}
